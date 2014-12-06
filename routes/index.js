b3d86e9c8adebb101a3dfa89aaad103f8938b3a6
Poojit Sharma <poojits@gmail.com>
2014-12-06 07:42:30 -0800

    fixed solr queries (via george)

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/cq1', function(req, res) {
 	var startDate = req.query.startDate;
 	var endDate = req.query.endDate;
 	var field = req.query.field;
-	var url = 'http://d3.poojit.com:8080/solr/oodt-fm/select?q=postedDate:['+startDate+'-01T00:00:00Z%20TO%20'+endDate+'-31T00:00:00Z]&facet=true&facet.pivot=geohash3,'+field+'&f.'+field+'.facet.limit=1&f.geohash3.facet.limit=300&wt=json&indent=false';
+	var url = 'http://d3.poojit.com:8080/solr/oodt-fm/select?q=*:*&fq=postedDate:['+startDate+'-01T00:00:00Z%20TO%20'+endDate+'-31T00:00:00Z]&facet=true&facet.pivot=geohash3,'+field+'&f.'+field+'.facet.limit=1&f.geohash3.facet.limit=300&wt=json&indent=false';
 	console.log('Solr URL: ' + url);
 	request(url, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
@@ -48,7 +48,7 @@ router.get('/cq2', function(req, res) {
 	var startDate = req.query.startDate;
 	var endDate = req.query.endDate;
 	var company_name = req.query.company_name;
-	var url = 'http://d3.poojit.com:8080/solr/oodt-fm/select?q=postedDate:['+startDate+'-01T00:00:00Z%20TO%20'+endDate+'-31T00:00:00Z]%20company:'+company_name+'&facet=true&facet.pivot=geohash4,geohash1&f.geohash4.facet.limit=100&f.geohash1.facet.limit=1&wt=json&indent=false';
+	var url = 'http://d3.poojit.com:8080/solr/oodt-fm/select?q=*:*&fq=postedDate:['+startDate+'-01T00:00:00Z%20TO%20'+endDate+'-31T00:00:00Z]%20company:'+company_name+'&facet=true&facet.pivot=geohash4,geohash1&f.geohash4.facet.limit=100&f.geohash1.facet.limit=1&wt=json&indent=false';
 	console.log('Solr URL: ' + url);
 	request(url, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
@@ -117,7 +117,7 @@ router.get('/cq4', function(req, res) {
 	var startDate = req.query.startDate;
 	var endDate = req.query.endDate;
 	var jobcat = req.query.jobcat;
-	var url = 'http://d3.poojit.com:8080/solr/oodt-fm/select?q=computer%20postedDate:['+startDate+'-01T00:00:00Z%20TO%20'+endDate+'-31T00:00:00Z]&facet=true&facet.pivot=geohash3,geohash1&f.geohash3.facet.limit=500&f.geohash1.facet.limit=1&wt=json&indent=false';
+	var url = 'http://d3.poojit.com:8080/solr/oodt-fm/select?q='+jobcat+'&fq=postedDate:['+startDate+'-01T00:00:00Z%20TO%20'+endDate+'-31T00:00:00Z]&facet=true&facet.pivot=geohash3,geohash1&f.geohash3.facet.limit=500&f.geohash1.facet.limit=1&wt=json&indent=false';
 
 	console.log('Solr URL: ' + url);
 	request(url, function (error, response, body) {
